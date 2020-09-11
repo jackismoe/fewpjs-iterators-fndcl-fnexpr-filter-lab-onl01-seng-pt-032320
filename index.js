7653c941f4cdbb2121b3d7c3646fc7852af6e763
jackismoe <jacksonmo@me.com>
2020-09-11 10:20:31 -0400

    Lab Submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+const findMatching = (array, match) => {
+  let driver = array.filter(possible => possible.toLowerCase() === match.toLowerCase())
+  return driver
+}
+
+const fuzzyMatch = (array, match) => {
+  let driver = array.filter(possible => possible.toLowerCase().indexOf(match.toLowerCase()) === 0 ) 
+  return driver
+}
+
+const matchName = (array, match) => {
+  let driver = array.filter(possible => possible.name === match)
+  return driver
+}
